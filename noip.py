90a560927fa201fa328ffc3210a7d315fe905a8a
Kelvin Steiner <kelvinsteinersantos@gmail.com>
2012-11-19 13:22:03 -0200

    fixed some bugs and added getip module

---
+++ noip.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+
+import sys
+import base64
+import urllib2
+
+
+# URL a ser formatada
+urlbase = "http://dynupdate.no-ip.com/nic/update?hostname={2}&myip={3}"
+
+
+# Atualiza um host
+def update(username, password, host, ip):
+	# Foramta a URL
+	url = urlbase.format(username, password, host, ip)
+	
+	# HTTP Request
+	
+	userData = "Basic " + (username + ":" + password).encode("base64").rstrip()
+	
+	req = urllib2.Request(url)
+	req.add_header("Authorization", userData)
+	
+	try:
+		res = urllib2.urlopen(req)
+	except urllib2.HTTPError:
+		#sprint "\nerro: ",
+		print "authentication error"
+		exit(2)
+	
+	# Retorna a respota
+	return res.read()
+
+
+# Mensagems exibidas para as respostas do No-IP
+messages = {
+		"good":    "[SUCESS] Host atualizado com sucesso.",
+		"nochg":   "[SUCESS] O Host já está atualizado.",
+		"nohost":  "[ERROR] O Host fornecido não existe.",
+		"badauth": "[ERROR] Combinação de usuário e senha inválida.",
+		"badagent":"[ERROR] \"Cliente desativado\"", # Não sei o significado :(
+		"!donator":"[ERROR] A solicitação de update foi enviada com"+
+			" um recurso que está indisponível para o usuário especificado.",
+		"abuse":   "[ERROR] O usuário está bloqueado.",
+		"911":     "[ERROR] Um erro fatal aconteceu."
+		}
+
+# Retorna uma mensagem de acordo com a resposta obtida do no-ip
+def get_response(response):
+	for m in range(len(messages.keys())):
+		key = messages.keys()[m]
+		message = messages[key]
+		if response.find(key) == 0:
+			return message
+
+
+# Atualiza um ou mais hosts, printando as mensagens
+def updateHosts(username, password, ip, hosts):
+	
+	# Se hosts for lista ou tupla
+	if type(hosts) in (list, tuple):
+		# Printa o IP
+		print "ip: {}".format(ip)
+		# Para cada host
+		for host in hosts:
+			# Printa o nome do host
+			print "\nhost: {}".format(host)
+			# Atualiza e printa a informação correspondente a resposta recebida
+			resp = update(username, password, host, ip)
+			print get_response(resp)
+	
+	# Se hosts for string
+	elif type(hosts) == str:
+		# Printa o IP
+		print "ip: {}".format(ip)
+		# Printa o nome do host
+		print "host: {}".format(hosts)
+		# Atualiza e printa a informação correspondente a resposta recebida
+		resp = update(username, password, hosts, ip)
+		print get_response(resp)
+
+
+

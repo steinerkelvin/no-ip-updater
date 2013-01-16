9da6819c0dc6da1373aff3b2ad50896882b0390a
Kelvin Steiner <kelvinsteinersantos@gmail.com>
2013-01-16 09:50:39 -0200

    windows setup error fixed and dyndns.org to get IP instead of whatsmyip

--- no_ip_updater/getip.py
+++ no_ip_updater/getip.py
@@ -2,15 +2,29 @@
 # coding: UTF-8
 
 import urllib2
+import re
 
 whatismyip_url = "http://automation.whatismyip.com/n09230945.asp"
+dyndns_url = "http://checkip.dyndns.org/"
 
-def get_ip():
+def whatismyip_get_ip():
 	"""
-	Retorna o IP externo atual
+	Retorna o IP externo atual, usando whatismyip
 	"""
 	ip = urllib2.urlopen(whatismyip_url).read()#.readlines()[0]
 	return ip
-
-
+	
+def dyndns_get_ip():
+	"""
+	Retorna o IP externo atual
+	"""
+	text = urllib2.urlopen(dyndns_url).read()#.readlines()[0]
+	
+	#ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', text )
+	ip = re.search( r'[0-9]+(?:\.[0-9]+){3}', text ).group()
+	
+	return ip
+	
+def get_ip():
+	return dyndns_get_ip()
 
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 
+#datafiles = [ ('/usr/bin/', ['update-no-ip']) ]
+datafiles=[ ('.', ['update-no-ip']) ]
+
 setup(name='no-ip-updater',
 	  version='1.0',
 	  description='Python Module to Update No-IP',
 	  author='Kelvin Steiner',
 	  author_email='kelvinsteinersantos@gmail.com',
 	  packages=["no_ip_updater"],
-	  data_files=[ ('/usr/bin/', ['update-no-ip']) ]
+	  data_files = datafiles
 	  )

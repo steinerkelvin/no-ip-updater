b2e8b73e0c49ffb28f4f72e61b0a5aebd6526cda
Kelvin Steiner <kelvinsteinersantos@gmail.com>
2013-02-06 11:29:18 -0200

    installing the executable script "update-no-ip" through "scripts" parameter instead of "datafiles"

--- setup.py
+++ setup.py
@@ -2,10 +2,6 @@ from distutils.core import setup
 
 import os
 
-if os.name == "posix":
-	datafiles = [ ('/usr/bin/', ['update-no-ip']) ]
-else:
-	datafiles=[ ('.', ['update-no-ip']) ]
 
 setup(name='no-ip-updater',
 	  version='1.0',
@@ -13,5 +9,5 @@ setup(name='no-ip-updater',
 	  author='Kelvin Steiner',
 	  author_email='kelvinsteinersantos@gmail.com',
 	  packages=["no_ip_updater"],
-	  data_files = datafiles
+	  scripts = ["update-no-ip"]
 	  )
